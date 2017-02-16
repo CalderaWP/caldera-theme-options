406998595523f57799cbf441ee70eb90882086f7
Roy Sivan <roy.sivan@disney.com>
2017-02-16 09:32:48 -0800

    Adding in Theme Options Page

--- classes/cmb2.php
+++ classes/cmb2.php
@@ -26,6 +26,10 @@ class cmb2{
         add_action( 'cmb2_admin_init', [ $this, 'make_boxes' ] );
         add_action( 'cmb2_admin_init', [ $this, 'add_download_fields' ], 25 );
         add_action( 'cmb2_admin_init', [ $this, 'add_common_fields' ], 30 );
+
+	    /** Options Page */
+	    add_action( 'admin_menu', [ $this, 'add_options_page' ] );
+	    add_action( 'cmb2_admin_init', [ $this, 'add_options_page_metabox' ] );
     }
 
     /**
@@ -148,4 +152,89 @@ class cmb2{
 
 
     }
+
+	public function add_options_page() {
+		$this->options_page = add_menu_page( 'CL Settings', 'CL Settings', 'manage_options', 'cl_settings', [ $this, 'admin_page_display' ] );
+		// Include CMB CSS in the head to avoid FOUC
+		//add_action( "admin_print_styles-{$this->options_page}", array( 'CMB2_hookup', 'enqueue_cmb_css' ) );
+	}
+	/**
+	 * Admin page markup. Mostly handled by CMB2
+	 * @since  0.1.0
+	 */
+	public function admin_page_display() {
+		?>
+		<div class="wrap cmb2-options-page <?php echo 'cl_settings'; ?>">
+			<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+			<?php cmb2_metabox_form( cl_settings, 'cl_settings' ); ?>
+		</div>
+		<?php
+	}
+	/**
+	 * Add the options metabox to the array of metaboxes
+	 * @since  0.1.0
+	 */
+	function add_options_page_metabox() {
+		// hook in our save notices
+		add_action( "cmb2_save_options-page_fields_{cl_settings}", array( $this, 'settings_notices' ), 10, 2 );
+		$cmb = new_cmb2_box( array(
+			'id'         => cl_settings,
+			'hookup'     => false,
+			'cmb_styles' => false,
+			'show_on'    => array(
+				// These are important, don't remove
+				'key'   => 'options-page',
+				'value' => array( 'cl_settings', )
+			),
+		) );
+		// Set our CMB2 fields
+		$cmb->add_field( array(
+			'name' => __( 'ROI Page', 'caldera_theme' ),
+			'desc' => __( 'Page for ROI section', 'caldera_theme' ),
+			'id'   => 'roi_page',
+			'type' => 'select',
+			'options_cb' => [ $this, 'get_pages']
+		) );
+	}
+
+	function get_pages( $field ) {
+
+		$pages = get_posts( ['post_type' => 'page'] );
+		$return_pages = [0 => 'Choose A Page'];
+		foreach( $pages as $page ) {
+			$return_pages[$page->ID] = $page->post_title;
+		}
+
+		return $return_pages;
+
+	}
+
+	/**
+	 * Register settings notices for display
+	 *
+	 * @since  0.1.0
+	 * @param  int   $object_id Option key
+	 * @param  array $updated   Array of updated fields
+	 * @return void
+	 */
+	public function settings_notices( $object_id, $updated ) {
+		if ( $object_id !== 'cl_settings' || empty( $updated ) ) {
+			return;
+		}
+		add_settings_error( 'cl_settings' . '-notices', '', __( 'Settings updated.', 'myprefix' ), 'updated' );
+		settings_errors( 'cl_settings' . '-notices' );
+	}
+	/**
+	 * Public getter method for retrieving protected/private variables
+	 * @since  0.1.0
+	 * @param  string  $field Field to retrieve
+	 * @return mixed          Field value or exception is thrown
+	 */
+	public function __get( $field ) {
+		// Allowed fields to retrieve
+		if ( in_array( $field, array( 'key', 'metabox_id', 'title', 'options_page' ), true ) ) {
+			return $this->{$field};
+		}
+		throw new Exception( 'Invalid property: ' . $field );
+	}
 }
